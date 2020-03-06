ec1a73c745604992a82b6b0e0975ac75d20b0987
JuliaBuslo <ulia.grichishuk@gmail.com>
2020-03-06 15:36:31 +0300

    Initial commit.

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// fix header
+// let header = document.querySelector('.header');
+
+// if(header) {
+//   window.onscroll = function(){
+//     if(document.documentElement.scrollTop > 590){
+//       header.classList.add("header-fixed");
+//     }
+//     else{
+//       header.classList.remove("header-fixed");
+//     }
+//   }
+// }
+
+// slik shedule 
+$(document).ready(function(){
+    $('.shedule-slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1
+    });
+  });
